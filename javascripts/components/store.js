b5584df7c190157709496a77c616f3f5118c4ce8
Dev Davis <sean.dev.davis@gmail.com>
2019-04-15 12:46:52 -0500

    got print to dom working

--- javascripts/components/store.js
+++ javascripts/components/store.js
@@ -2,7 +2,26 @@ import meat from './meats.js';
 import util from './../helpers/util.js';
 
 const makeShop = () => {
-    let meatInfo = meat.getMeats();
+    const meatInfo = meat.getMeats();
+
+    let domString = `<h2>Select your meats</h2>`;
+    domString += `<input type="checkbox" name="meat" id="chicken">`
+    domString += `<label for="chicken"> &nbsp Chicken: </label>`
+    domString += `<br>`
+    domString += `<input type="checkbox" name="meat" id="ham">`
+    domString += `<label for="ham"> &nbsp Ham: </label>`
+    domString += `<br>`
+    domString += `<input type="checkbox" name="meat" id="turkey">`
+    domString += `<label for="turkey">  &nbsp Turkey: </label>`
+    domString += `<br>`
+    domString += `<input type="checkbox" name="meat" id="salami">`
+    domString += `<label for="salami">  &nbsp Salami: </label>`
+    domString += `<br>`
+    domString += `<input type="checkbox" name="meat" id="bologna">`
+    domString += `<label for="bologna"> &nbsp Bologna: </label>`
+    domString += `<br>`
+
+    util.printToDom('meats', domString)
 };
 
 export default { makeShop };
---
+++ javascripts/helpers/eventListeners.js
@@ -0,0 +1,13 @@
+
+const checkboxType = document.querySelector('checkbox');
+const chicken = getElementById('chicken');
+const sandwich = [];
+
+
+
+const checkboxChecker = () => {
+    if(checkboxType === chicken) {
+
+    }
+}
+chicken.addEventListener
